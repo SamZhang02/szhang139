91aac68a4e69faab81263406716117bfb0b42b6d
SamZhang02 <sam.zijing.zhang@gmail.com>
2023-03-02 21:27:11 -0500

    add social media logos

--- src/App.tsx
+++ src/App.tsx
@@ -23,6 +23,7 @@ function Header() {
     <header className="App-header">
       <Title/>
       <SubTitle/>
+      <SocialLinks/>
     </header>
   )
 }
@@ -59,7 +60,7 @@ function SubTitle(){
   return (
     <div>
       <div className="">
-        Computer Science and Statistics student at <span className='text-decoration-line: underline'>McGill University</span>.
+        Computer Science and Statistics student at <span className='hover:underline'><a href="https://mcgill.ca/">McGill University</a></span>.
       </div>
     </div>
   )
@@ -159,5 +160,26 @@ function TrademarkBar() {
     </div>
   );
 }
+
+function SocialLinks() {
+  return (
+    <div className="flex justify-center">
+      <SocialLink name={"GitHub"}image={"github.png"} link={"https://www.github.com/SamZhang02"}/>
+      <SocialLink name={"LinkedIn"}image={"linkedin.png"} link={"https://www.linkedin.com/in/zhang-sam"}/>
+    </div>
+  );
+}
+
+function SocialLink({name, image, link}: {name:string, image:string, link:string}) {
+  return (
+    <a href={link}>
+      <div className="flex-col items-center justify-center p-3 group">
+        <div className='flex justify-center items-center'>
+          <img className="h-10 w-10 relative mx-auto top-3 group-hover:top-0 duration-150" src={MEDIAPATH + image}></img>
+        </div>
+        <p className='text-sm opacity-0 relative top-0 group-hover:top-2 group-hover:opacity-100 duration-150'>{name}</p>
+    </div>
+  </a>
+)}
 
 export default App;
